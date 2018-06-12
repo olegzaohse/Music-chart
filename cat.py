40bb18c239bda60c7a5835c58e73c27efa73a34a
olegzaohse <39778091+olegzaohse@users.noreply.github.com>
2018-06-12 13:27:26 +0300

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,14 @@
+import command_system
+import vkapi
+
+def cat():
+   # Получаем случайную картинку из паблика
+   attachment = vkapi.get_random_wall_picture(-32015300)
+   message = 'Вот тебе котик :)\nВ следующий раз я пришлю другого котика.'
+   return message, attachment
+
+cat_command = command_system.Command()
+
+cat_command.keys = ['котик', 'кошка', 'кот', 'котенок', 'котяра', 'cat']
+cat_command.description = 'Пришлю картинку с котиком'
+cat_command.process = cat
---
+++ settings.py
@@ -0,0 +1,2 @@
+token = '4ce2eb9d3d9f6f60c2d1e8d3019452da423a43b6b7e3eb418335b8e9a8d0b2a0e6cfd428523ef6f541a95'
+confirmation_token = 'a8e58c58'
